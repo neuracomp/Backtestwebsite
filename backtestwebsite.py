a62981a647c7cc0b2874de4391d782fd27a82019
neuracomp <146193703+neuracomp@users.noreply.github.com>
2024-06-03 22:24:16 -0500

    Update backtestwebsite.py

--- backtestwebsite.py
+++ backtestwebsite.py
@@ -151,9 +151,9 @@ def optimize_rsi(ticker, start_date, end_date):
         best_return = float('-inf')
         
         param_combinations = [(window, entry_rsi, exit_rsi) 
-                              for window in range(10, 31, 5) 
-                              for entry_rsi in range(0, 51, 5) 
-                              for exit_rsi in range(50, 101, 5)]
+                              for window in range(10, 31, 1) 
+                              for entry_rsi in range(0, 51, 1) 
+                              for exit_rsi in range(50, 101, 1)]
         
         progress_bar = st.progress(0)
         total_combinations = len(param_combinations)
